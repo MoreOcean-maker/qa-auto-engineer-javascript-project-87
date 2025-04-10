16163184d00c2c0916d3364d91c28b340ab7455a
igor <moreworkshop320@gmail.com>
2025-04-10 21:10:36 +0300

    донастроил CLI по 2 этапу проекта

--- gendiff.js
+++ gendiff.js
@@ -5,7 +5,10 @@ import { Command } from 'commander';
 const program = new Command();
 
 program
+  .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
-  .helpOption('-h, --help', 'display help for command')
+  .option('-f, --format <type>', 'output format') 
+  .arguments('<filepath1> <filepath2>')
   .parse(process.argv);
+
