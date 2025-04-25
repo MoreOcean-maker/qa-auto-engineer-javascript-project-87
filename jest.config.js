00d53f92d3e1011f80d501cefec16fe527605236
igor <moreworkshop320@gmail.com>
2025-04-25 19:10:25 +0300

    Завершил 6 этап 6 попытка

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+export default {
+	transform: {},
+	testEnvironment: 'node',
+	moduleFileExtensions: ['js', 'json', 'jsx', 'node'],
+	transformIgnorePatterns: [
+	  '/node_modules/(?!your-esm-package-to-transform)',
+	],
+  };
+  
