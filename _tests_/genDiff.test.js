a6a420d51530cbd1c3030d2ccd849de32a732a5a
igor <moreworkshop320@gmail.com>
2025-05-05 00:22:49 +0300

    Пройден 7 этап 4 попытка

--- _tests_/genDiff.test.js
+++ _tests_/genDiff.test.js
@@ -6,19 +6,10 @@ const dirname = path.dirname(fileURLToPath(import.meta.url));
 const getFixturePath = (filename) => path.join(dirname, '..', '_fixtures_', filename);
 
 describe('genDiff', () => {
-  test('correctly compares two JSON files', () => {
+  test('plain format comparison', () => {
     const file1Path = getFixturePath('file1.json');
     const file2Path = getFixturePath('file2.json');
-    const result = genDiff(file1Path, file2Path);
-    expect(result).toMatchInlineSnapshot(`
-      "{
-          host: hexlet.io
-        - timeout: 50
-        + timeout: 20
-        - proxy: 123.234.53.22
-        - follow: false
-        + verbose: true
-      }"
-    `);
+    const result = genDiff(file1Path, file2Path, 'plain');
+    expect(result).toMatchSnapshot();
   });
 });
