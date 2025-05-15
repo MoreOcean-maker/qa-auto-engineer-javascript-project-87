821069dce24c535b1a86e52146669d9dea27a111
igor <moreworkshop320@gmail.com>
2025-05-15 13:57:48 +0300

    Исправил недоработки по проекту 1

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
-import parseFile from '../src/parsers.js';
 import genDiff from '../src/genDiff.js';
 
 const program = new Command();
@@ -11,11 +10,15 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
   .arguments('<filepath1> <filepath2>')
-  .option('-f, --format <type>', 'output format')
-  .action((filepath1, filepath2) => {
-    const data1 = parseFile(filepath1);
-    const data2 = parseFile(filepath2);
-    genDiff(data1, data2);
+  .option('-f, --format <type>', 'output format', 'stylish')
+  .action((filepath1, filepath2, options) => {
+    try {
+      const diff = genDiff(filepath1, filepath2, options.format);
+      console.log(diff);
+    } catch (error) {
+      console.error(error.message);
+      process.exit(1);
+    }
   });
 
 program.parse(process.argv);
--- src/buildDiff.js
+++ src/buildDiff.js
@@ -3,29 +3,52 @@ const buildDiff = (data1, data2) => {
 
   return keys.map((key) => {
     if (!Object.hasOwn(data1, key)) {
-      return { key, type: 'added', value: data2[key] };
+      return {
+        key,
+        type: 'added',
+        value: data2[key],
+      };
     }
 
     if (!Object.hasOwn(data2, key)) {
-      return { key, type: 'removed', value: data1[key] };
+      return {
+        key,
+        type: 'removed',
+        value: data1[key],
+      };
     }
 
     const value1 = data1[key];
     const value2 = data2[key];
 
-    if (typeof value1 === 'object' && value1 !== null
-     && typeof value2 === 'object' && value2 !== null) {
-      return { key, type: 'nested', children: buildDiff(value1, value2) };
+    if (
+      typeof value1 === 'object' &&
+      value1 !== null &&
+      typeof value2 === 'object' &&
+      value2 !== null
+    ) {
+      return {
+        key,
+        type: 'nested',
+        children: buildDiff(value1, value2),
+      };
     }
 
     if (value1 !== value2) {
-      return { key, type: 'updated', oldValue: value1, value: value2 };
+      return {
+        key,
+        type: 'updated',
+        oldValue: value1,
+        value: value2,
+      };
     }
 
-    return { key, type: 'unchanged', value: value1 };
+    return {
+      key,
+      type: 'unchanged',
+      value: value1,
+    };
   });
 };
 
 export default buildDiff;
-
-
