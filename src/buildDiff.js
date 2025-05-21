d041bd7ac8ad2c2c14d240f9cc9c0bd21b2f4a84
igor <moreworkshop320@gmail.com>
2025-05-21 18:56:01 +0300

    Исправил все замечания по проекту 2

--- src/buildDiff.js
+++ src/buildDiff.js
@@ -1,37 +1,44 @@
-const hasKey = (obj, key) => Object.prototype.hasOwnProperty.call(obj, key)
+const createNode = (key, type, value1, value2, children) => {
+  const node = { key, type };
+  if (children) {
+    node.children = children;
+  } else if (type === 'updated') {
+    node.oldValue = value1;
+    node.value = value2;
+  } else {
+    node.value = value1 !== undefined ? value1 : value2;
+  }
+  return node;
+};
 
 const buildDiff = (data1, data2) => {
-  const keys = Array.from(new Set([...Object.keys(data1), ...Object.keys(data2)])).sort()
-
-  const makeNode = (key, type, extras = {}) => ({ key, type, ...extras })
+  const keys = Array.from(new Set([...Object.keys(data1), ...Object.keys(data2)])).sort();
 
   return keys.map((key) => {
-    const inData1 = hasKey(data1, key)
-    const inData2 = hasKey(data2, key)
-
-    if (!inData1) {
-      return makeNode(key, 'added', { value: data2[key] })
+    if (!Object.prototype.hasOwnProperty.call(data1, key)) {
+      return createNode(key, 'added', undefined, data2[key]);
     }
 
-    if (!inData2) {
-      return makeNode(key, 'removed', { value: data1[key] })
+    if (!Object.prototype.hasOwnProperty.call(data2, key)) {
+      return createNode(key, 'removed', data1[key], undefined);
     }
 
-    const value1 = data1[key]
-    const value2 = data2[key]
+    const value1 = data1[key];
+    const value2 = data2[key];
 
-    const areObjects = val => typeof val === 'object' && val !== null
-
-    if (areObjects(value1) && areObjects(value2)) {
-      return makeNode(key, 'nested', { children: buildDiff(value1, value2) })
+    if (
+      typeof value1 === 'object' && value1 !== null &&
+      typeof value2 === 'object' && value2 !== null
+    ) {
+      return createNode(key, 'nested', undefined, undefined, buildDiff(value1, value2));
     }
 
     if (value1 !== value2) {
-      return makeNode(key, 'updated', { oldValue: value1, value: value2 })
+      return createNode(key, 'updated', value1, value2);
     }
 
-    return makeNode(key, 'unchanged', { value: value1 })
-  })
-}
+    return createNode(key, 'unchanged', value1, undefined);
+  });
+};
 
-export default buildDiff
+export default buildDiff;
