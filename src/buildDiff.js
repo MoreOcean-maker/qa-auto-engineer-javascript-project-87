231fbc857c3b90715f208fbe6bf9b1127d5ff5b7
igor <moreworkshop320@gmail.com>
2025-05-21 18:48:11 +0300

    Исправил все замечания по проекту 1

--- src/buildDiff.js
+++ src/buildDiff.js
@@ -1,53 +1,36 @@
+const hasKey = (obj, key) => Object.prototype.hasOwnProperty.call(obj, key)
+
 const buildDiff = (data1, data2) => {
   const keys = Array.from(new Set([...Object.keys(data1), ...Object.keys(data2)])).sort()
 
+  const makeNode = (key, type, extras = {}) => ({ key, type, ...extras })
+
   return keys.map((key) => {
-    if (!Object.prototype.hasOwnProperty.call(data1, key)) {
-      return {
-        key,
-        type: 'added',
-        value: data2[key],
-      }
+    const inData1 = hasKey(data1, key)
+    const inData2 = hasKey(data2, key)
+
+    if (!inData1) {
+      return makeNode(key, 'added', { value: data2[key] })
     }
 
-    if (!Object.prototype.hasOwnProperty.call(data2, key)) {
-      return {
-        key,
-        type: 'removed',
-        value: data1[key],
-      }
+    if (!inData2) {
+      return makeNode(key, 'removed', { value: data1[key] })
     }
 
     const value1 = data1[key]
     const value2 = data2[key]
 
-    if (
-      typeof value1 === 'object'
-      && value1 !== null
-      && typeof value2 === 'object'
-      && value2 !== null
-    ) {
-      return {
-        key,
-        type: 'nested',
-        children: buildDiff(value1, value2),
-      }
+    const areObjects = val => typeof val === 'object' && val !== null
+
+    if (areObjects(value1) && areObjects(value2)) {
+      return makeNode(key, 'nested', { children: buildDiff(value1, value2) })
     }
 
     if (value1 !== value2) {
-      return {
-        key,
-        type: 'updated',
-        oldValue: value1,
-        value: value2,
-      }
+      return makeNode(key, 'updated', { oldValue: value1, value: value2 })
     }
 
-    return {
-      key,
-      type: 'unchanged',
-      value: value1,
-    }
+    return makeNode(key, 'unchanged', { value: value1 })
   })
 }
 
